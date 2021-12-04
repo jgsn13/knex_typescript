4e7b4d7d70a13630f288fe1fef6c94f3510929a2
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-04 18:07:04 -0300

    providing static content

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { resolve } from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(routes);
+
+// Providing static content from the uploads folder in the /uploads route.
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 const port = 3333;
 
