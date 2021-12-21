381807e9d2ccce2492972bf22c0fec6b289c58cf
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-21 15:15:53 -0300

    list locations

--- src/routes/locations.routes.ts
+++ src/routes/locations.routes.ts
@@ -3,6 +3,65 @@ import knex from '../database/connection';
 
 const locationsRouter = Router();
 
+locationsRouter.get('/', async (request, response) => {
+  try {
+    const { city, uf, items } = request.query;
+
+    // Parsing items id from items query param
+    const parsedItems = String(items)
+      .split(',')
+      .map((item) => Number(item.trim()));
+
+    const locations = await knex('locations')
+      .join('location_items', 'locations.id', '=', 'location_items.location_id')
+      .whereIn('location_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('locations.*');
+
+    return response.json(locations);
+  } catch (error) {
+    console.log({ error });
+    return response.status(400).json({ error });
+  }
+});
+
+locationsRouter.get('/:id', async (request, response) => {
+  try {
+    const { id } = request.params;
+    const trx = await knex.transaction();
+
+    try {
+      const location = await knex('locations')
+        .transacting(trx)
+        .where('id', id)
+        .first();
+
+      if (!location) throw 'Location not found.';
+
+      const items = await knex('items')
+        .transacting(trx)
+        .join('location_items', 'items.id', '=', 'location_items.item_id')
+        .where('location_items.location_id', id)
+        .select('items.title');
+
+      trx.commit();
+
+      return response.json({
+        ...location,
+        items,
+      });
+    } catch (err) {
+      trx.rollback();
+      throw err;
+    }
+  } catch (error) {
+    console.log({ error });
+    return response.status(400).json({ error });
+  }
+});
+
 locationsRouter.post('/', async (request, response) => {
   try {
     const { name, email, whatsapp, latitude, longitude, city, uf, items } =
@@ -53,41 +112,6 @@ locationsRouter.post('/', async (request, response) => {
     } catch (err) {
       trx.rollback();
 
-      throw err;
-    }
-  } catch (error) {
-    console.log({ error });
-    return response.status(400).json({ error });
-  }
-});
-
-locationsRouter.get('/:id', async (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const trx = await knex.transaction();
-    try {
-      const location = await knex('locations')
-        .transacting(trx)
-        .where('id', id)
-        .first();
-
-      if (!location) throw 'Location not found.';
-
-      const items = await knex('items')
-        .transacting(trx)
-        .join('location_items', 'items.id', '=', 'location_items.item_id')
-        .where('location_items.location_id', id)
-        .select('items.title');
-
-      trx.commit();
-
-      return response.json({
-        ...location,
-        items,
-      });
-    } catch (err) {
-      trx.rollback();
       throw err;
     }
   } catch (error) {
